9ffcde8852d7c8f4b71819446fb883c08490ca98
Bryan Padron <Bpadron2000@gmail.com>
2017-10-04 00:13:24 -0400

    replaces es6 with es5

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,7 +2,7 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
-window.onload = () => {
+window.onload = function (){
   container = document.getElementById("container")
 
   div = document.createElement("div")
