9721b2c948cad4650a60eeb753850067a2e06cba
Bryan Padron <Bpadron2000@gmail.com>
2017-10-18 02:18:42 -0400

    adds config.yaml to gitignore

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,14 @@ module Tpbot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    dev = File.join(Rails.root, 'config', 'config.yml')
-    YAML.load(File.open(dev))["defaults"].each do |key,value|
-      ENV[key.to_s] = value
-    end
+    # ENV["tp_auth_user"] = Rails.env.tp_auth_user
+    # ENV["tp_auth_pass"] = Rails.env.tp_auth_pass
+    # ENV["tp_auth_token"] = Rails.env.tp_auth_token
+    # ENV["harvest_auth_token"] = Rails.env.harvest_auth_token
+    # ENV["slack_auth_token"] = Rails.env.slack_auth_token
+    # YAML.load(File.open(dev))["defaults"].each do |key,value|
+    #   ENV[key.to_s] = value
+    # end
     config.allow_concurrency = true
     config.autoload_paths << Rails.root.join('lib')
     config.eager_load_paths  += [Rails.root.join('lib').to_s]
