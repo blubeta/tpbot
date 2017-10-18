4450846bdfd087636c0a01a5be214e20eef274c3
Bryan Padron <Bpadron2000@gmail.com>
2017-10-18 02:50:20 -0400

    mend

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,6 @@ module Tpbot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # ENV["tp_auth_user"] = Rails.env.tp_auth_user
-    # ENV["tp_auth_pass"] = Rails.env.tp_auth_pass
-    # ENV["tp_auth_token"] = Rails.env.tp_auth_token
-    # ENV["harvest_auth_token"] = Rails.env.harvest_auth_token
-    # ENV["slack_auth_token"] = Rails.env.slack_auth_token
     # YAML.load(File.open(dev))["defaults"].each do |key,value|
     #   ENV[key.to_s] = value
     # end
